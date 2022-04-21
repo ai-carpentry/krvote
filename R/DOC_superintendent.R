2e6f9fda21d72231d292ed4047b5ce82e6624288
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2022-04-21 16:36:37 +0900

    added 교육감 2018

---
+++ R/DOC_superintendent.R
@@ -0,0 +1,22 @@
+# 교육감 선거 ----------------------
+
+#' @title 제7회 교육감선거
+#' @description 시도 교육감
+#' @format A data frame with 54305 rows and 10 variables:
+#' \describe{
+#'   \item{\code{시도명}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{구시군}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{읍면동명}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{구분}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{선거인수}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{투표수}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{무효투표수}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{기권수}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{후보}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{득표수}}{double COLUMN_DESCRIPTION}
+#'}
+#' @source \url{http://info.nec.go.kr/main/main_load.xhtml}
+#' @details 제7회 시도 교육감
+"education_2018"
+
+
